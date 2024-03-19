23ed65b2132cf87a837e687584a84ab7b503bcc7
Rasyidan Akbar F <78522797+0xrsydn@users.noreply.github.com>
2024-03-19 19:23:26 +0700

    Update app.py
    Remove OpenAI api key from langchain function (user input), code is specifically run on local

--- app.py
+++ app.py
@@ -1,29 +1,33 @@
 import gradio as gr
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-import os
 from langchain_core.output_parsers import StrOutputParser
 from langchain.chains import LLMChain
+import os
 
-#Get the OpenAI API key from the environment
-openai_api_key = os.environ.get("OPENAI_API_KEY")
-
-# Define the chain and LLM first
-model = ChatOpenAI()
-prompt = PromptTemplate.from_template("""
-So, I am applying for {job_role} at {company_name}
-=================
-{company_context}
-=================
-{candidate_profile}
-=================
-From the company profile and my profile, please create a cover letter for the {job_role} position. Ensure that it is well-crafted and engaging for recruiters and hiring managers. Also, verify that my recent work experience and academic background align with the role I am applying for.
-""")
-output_parser = StrOutputParser()
-chain = LLMChain(llm=model, prompt=prompt)
+# Set the API key as an environment variable
+os.environ["OPENAI_API_KEY"]
 
 # Create Gradio interface function with topic as input
 def generate_cover_letter(job_role, company_name, company_context, candidate_profile):
+
+    # Load the LLM model
+    model_name = "gpt-3.5-turbo"
+    llm = ChatOpenAI(model_name=model_name)
+
+    # Define the chain and LLM
+    prompt = PromptTemplate.from_template("""
+    So, I am applying for {job_role} at {company_name}
+    =================
+    {company_context}
+    =================
+    {candidate_profile}
+    =================
+    From the company profile and my profile, please create a cover letter for the {job_role} position. Ensure that it is well-crafted and engaging for recruiters and hiring managers. Also, verify that my recent work experience and academic background align with the role I am applying for.
+    """)
+    output_parser = StrOutputParser()
+    chain = LLMChain(llm=llm, prompt=prompt)
+
     output = chain.run(job_role=job_role, company_name=company_name, company_context=company_context, candidate_profile=candidate_profile)
     return output
 
@@ -36,7 +40,7 @@ gr.Interface(
         gr.Textbox(label="Company Context", placeholder="Enter a brief description of the company"),
         gr.Textbox(label="Candidate Profile", placeholder="Describe your professional background, key skills, and relevant experiences")
     ],
-    outputs=gr.Textbox(label="Generated Cover Letter"),
+    outputs=gr.Textbox(label="Generated Cover Letter", show_copy_button=True),
     title="Cover Letter Generator",
     description="Generate a cover letter based on your job role, company, context, and profile."
 ).launch()
